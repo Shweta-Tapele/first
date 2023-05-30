4e82c8f69431481ea8498923b96f06e2a2767dae
Shweta-Tapele <shwetatapele93@gmail.com>
2023-05-30 15:10:12 -0700

    switchcase statement

---
+++ switchcase.js
@@ -0,0 +1,29 @@
+// Mon 7 am
+// Tues 8 am
+// Wed Thursday 6
+// Friday 9 am
+//Sat  sun 10 
+
+
+
+
+let day = "Sunday"
+
+switch(day)
+{
+    case 'Monday':
+        console.log("7 am");
+        break;
+    case 'Tuesday':
+        console.log("7 am");
+        break;
+    case 'Wednesday':
+        console.log("8 am");
+        break;
+    case "Thursday":
+        console.log("9 am");
+        break;
+    default :
+        console.log("10 am");
+
+}
