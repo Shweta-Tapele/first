9ec22c4b1b6adf4e35b5ac9b2b22ed7de3ecb7da
Shweta-Tapele <shwetatapele93@gmail.com>
2023-05-29 23:47:12 -0700

    first committed

---
+++ first.js
@@ -0,0 +1,36 @@
+// variables in Javascript
+//let is used to specify variables 
+// String variables should be specified using double quotes or single quotes 
+/*let num= 2+3
+console.log(num)
+
+console.log(num + 3)
+
+
+let username = 'Shweta'
+console.log(username)*/
+
+
+let radius = 5 
+const pi= 3.14
+
+
+let area 
+
+
+area = pi * radius * radius
+
+console.log(area)
+
+
+/*
+Datatypes can be ==> Primitive  and ===> Object
+
+*/
+
+
+
+
+let num1 = 7
+console.log(typeof num1)
+
